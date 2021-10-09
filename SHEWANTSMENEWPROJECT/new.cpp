265e796898f53fa75200f40a856df77d21fd5d63
SHEWANTSME <qhdmswns@mju.ac.kr>
2021-10-09 14:18:53 +0900

    뀨 \

--- SHEWANTSMENEWPROJECT/new.cpp
+++ SHEWANTSMENEWPROJECT/new.cpp
@@ -4,15 +4,21 @@ using namespace std;
 
 int main() {
 
-	int a,i, sum = 0;
-	cin >> a;
-	cout << "1";
-	for (i = 2; i < a; i++) {
-		if (a%i == 0) {
-			sum = sum + i;
-			
-			cout <<  " + "<<i; }
-		}
-	cout <<" = " << sum+1;
+	int number,i,tember;
+	int min = 1000, max = -1000; // max를 작은숫자, min을 클 숫자로 해야 
+
+	cin >> number;
+
+	for (i = 1; i < number + 1; i++) {
+		cin >> tember;
+		//if (i == 1) min = tember;
+		 if (tember < min) min=tember;
+		 if (tember > max) max=tember;
+	
+			}
+	cout << max - min << endl;
+	
 	return 0;
 }
+//이번문제는 생각해보니 그렇게 많이 해둘것이 없는게,, 굳이 쟤네들을 배열 만들어서 정렬한다음에 min max 찾는게아니라
+//그냥  5 2 3 1 6   이러면,, 5 가 min이 되고 
